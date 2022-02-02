c5225e26cb9cc8946bbbd6619071c85ba7eeb60f
SiriratWisessiri <95263943+SiriratWisessiri@users.noreply.github.com>
2022-02-02 20:20:32 +1100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -83,23 +83,7 @@ if(event.target.classList.contains("done-button")){
         taskManager.render();
     }
     });
- 
-/*     // Select the Tasks List
-const tasksList = document.querySelector('#task-list');
-// Add an 'onclick' event listener to the Tasks List
-tasksList.addEventListener('click', (event) => {
-    // Check if a "Mark As Done" button was clicked
-    if (event.target.classList.contains('done-button')) {
-        // Get the parent Task
-        const parentTask = event.target.parentElement.parentElement.parentElement;
-        // Get the taskId of the parent Task.
-        const taskId = Number(parentTask.dataset.taskId);
-        // Get the task from the TaskManager using the taskId
-        const task = taskManager.getTaskById(taskId);
-        // Update the task status to 'DONE'
-        task.status = 'Done';
-    } */
-  
+
     
 
 // Try delete button function that doesn't work
